ef391f807f6b3d32f6ee74da876bb435b9781afb
ravi123450 <141273837+ravi123450@users.noreply.github.com>
2025-01-19 22:02:51 +0530

    Update settings.py

--- cyberproj/settings.py
+++ cyberproj/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'assets/static')]
 django_heroku.settings(locals())
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
